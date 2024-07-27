5e2fc79b540891bbee4b43697c902acf6e5c8e62
VladimirPETROW <VladimirPETROW@yandex.ru>
2024-07-27 16:57:16 +0300

    Сортировка.

--- src/main/java/doc/test1/Folders.java
+++ src/main/java/doc/test1/Folders.java
@@ -1,49 +1,140 @@
 package doc.test1;
 
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class Folders {
 
     Path root;
     Path out;
-    HashMap<String, Part> parts;
+    HashMap<String, Part> namePart;
+    Part[] parts;
 
     public static void main(String[] args) {
         Path root = Paths.get(args[0]);
         Path out = Paths.get(args[1]);
         Folders folders = new Folders(root, out);
         folders.readFiles();
+        folders.sort();
+        folders.require();
+        folders.save();
     }
 
     public Folders(Path r, Path o) {
         root = r;
         out = o;
-        parts = new HashMap<>();
+        namePart = new HashMap<>();
     }
 
-    public void readFiles() {
+    void readFiles() {
         try {
+            int buffSize = 1024;
+            char[] buffer = new char[buffSize];
             Files.walkFileTree(root, new SimpleFileVisitor<Path>() {
                 @Override
                 public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
                     String name = file.getFileName().toString();
-                    if (parts.containsKey(name)) {
+                    if (namePart.containsKey(name)) {
                         throw new RuntimeException(String.format("Файл с таким именем уже существует: %s", file));
                     }
-                    List<String> lines = Files.readAllLines(file);
-                    Part part = new Part(name, file, lines);
-                    parts.put(part.name, part);
+                    StringBuffer text = new StringBuffer();
+                    try (FileReader reader = new FileReader(file.toFile())) {
+                        int count;
+                        while ((count = reader.read(buffer)) > 0) {
+                            text.append(buffer, 0, count);
+                        }
+                    }
+                    Part part = new Part(name, file, text.toString(), root);
+                    namePart.put(part.name, part);
                     return FileVisitResult.CONTINUE;
                 }
             });
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
 
+    void sort() {
+        parts = namePart.keySet().stream()
+                .sorted()
+                .map(name -> namePart.get(name))
+                .toArray(size -> new Part[size]);
+    }
+
+    void require() {
+        Part part = parts[0];
+        for (int i = 1; i < parts.length; i++) {
+            part = part.append(parts[i]);
+        }
+
+        for (int i = 0; i < parts.length; i++) {
+            part = parts[i];
+            for (String reqName : part.require) {
+                Part reqPart = namePart.get(reqName);
+                if (reqPart.isBefore(part)) continue;
+                reqPart.moveBefore(part);
+            }
+        }
+
+        for (int i = 0; i < parts.length; i++) {
+            part = parts[i];
+            for (String reqName : part.require) {
+                Part reqPart = namePart.get(reqName);
+                if (reqPart.isBefore(part)) continue;
+                StringBuffer message = new StringBuffer();
+                message.append("Обнаружена циклическая зависимость:");
+                message.append(String.format("\r\n%s require %s", part.file, reqPart.file));
+                ArrayList<Part> prevs = new ArrayList<>();
+                prevs.add(part);
+                Part np = part;
+                while ((np = np.next) != reqPart.next) {
+                    boolean req = false;
+                    for (Part prev : prevs) {
+                        if (np.isRequire(prev)) {
+                            message.append(String.format("\r\n%s require %s", np.file, prev.file));
+                            req = true;
+                        }
+                    }
+                    if (req) {
+                        prevs.add(np);
+                    }
+                }
+                throw new RuntimeException(message.toString());
+            }
+        }
+
+        Part first = parts[0];
+        while ((part = first.prev) != null) {
+            first = part;
+        }
+        part = first;
+        for (int i = 0; i < parts.length; i++) {
+            parts[i] = part;
+            part = part.next;
+        }
+    }
+
+    void save() {
+        StringBuffer message = new StringBuffer();
+        message.append("Файлы отсортированы в следующем порядке:\r\n");
+        for (Part part : parts) {
+            message.append(part.file);
+            message.append("\r\n");
+        }
+        System.out.print(message);
+
+        try (FileWriter writer = new FileWriter(out.toFile())) {
+            for (Part part : parts) {
+                writer.write(part.text);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
--- src/main/java/doc/test1/Part.java
+++ src/main/java/doc/test1/Part.java
@@ -1,8 +1,7 @@
 package doc.test1;
 
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,30 +10,77 @@ public class Part {
     String name;
     Path file;
     String text;
-    ArrayList<String> require;
+    String[] require;
+
+    Part prev, next;
 
     static Pattern pattern = Pattern.compile("require\\s+'([^']*)'");
 
-    public Part(String n, Path f, List<String> lines) {
+    public Part(String n, Path f, String t, Path root) {
         name = n;
-        file = f;
-        StringBuffer buffer = new StringBuffer();
-        ArrayList<String> req = new ArrayList<>(lines.size());
-        for (String line : lines) {
-            StringBuffer lb = new StringBuffer(line.length());
-            Matcher matcher = pattern.matcher(line);
-            while (matcher.find()) {
-                matcher.appendReplacement(lb, "");
-                String other = matcher.group(1);
-                req.add(other);
+        file = root.relativize(f);
+        HashSet<String> req = new HashSet<>();
+        StringBuffer buffer = new StringBuffer(t.length());
+        Matcher matcher = pattern.matcher(t);
+        while (matcher.find()) {
+            matcher.appendReplacement(buffer, "");
+            String require = matcher.group(1);
+            Path other = root.resolve(require);
+            if (!other.toFile().exists()) {
+                throw new RuntimeException(String.format("Файл из директивы require не найден %s", other));
             }
-            matcher.appendTail(lb);
-            if (lb.length() > 0) {
-                buffer.append(lb);
-                buffer.append("\r\n");
+            String otherName = other.getFileName().toString();
+            if (otherName.equals(n)) {
+                throw new RuntimeException(String.format("Файл содержит директиву require сам на себя %s", f));
+            }
+            req.add(otherName);
+        }
+        matcher.appendTail(buffer);
+        text = buffer.toString();
+        require = req.stream().sorted().toArray(size -> new String[size]);
+    }
+
+    public Part append(Part part) {
+        next = part;
+        part.prev = this;
+        return part;
+    }
+
+    public boolean isBefore(Part part) {
+        while ((part = part.prev) != null) {
+            if (part.name.equals(name)) return true;
+        }
+        return false;
+    }
+
+    public boolean isRequire(Part part) {
+        return Arrays.binarySearch(require, part.name) >= 0;
+    }
+
+    public void moveBefore(Part part) {
+        remove();
+        if (part.prev != null) {
+            part.prev.append(this);
+        }
+        this.append(part);
+    }
+
+    public void remove() {
+        if (prev != null) {
+            if (next != null) {
+                prev.append(next);
+                next = null;
+            }
+            else {
+                prev.next = null;
+            }
+            prev = null;
+        }
+        else {
+            if (next != null) {
+                next.prev = null;
+                next = null;
             }
         }
-        text = buffer.toString();
-        require = req;
     }
 }
